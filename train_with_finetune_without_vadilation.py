ec5773268e2f8990a3c635ce3ddaaf6d0799c9f6
Ultimo <2140777807@qq.com>
2024-01-15 21:28:11 +1100

    code modify

--- train_with_finetune_without_vadilation.py
+++ train_with_finetune_without_vadilation.py
@@ -264,8 +264,8 @@ def init_argument():
     parser.add_argument('--model_dir', default='./caoling_QA_model')
 
     parser.add_argument('--num_epoch', default=3000, help='number of epoch')
-    parser.add_argument('--batch_size', default=16, help='batch size')
-    parser.add_argument('--lr', default=2e-5, help='learning rate')
+    parser.add_argument('--batch_size', default=24, help='batch size')
+    parser.add_argument('--lr', default=2e-4, help='learning rate')
     parser.add_argument('--data_parallel', default=False)
     parser.add_argument('--max_len', default=25, help='max length of inputs')
     parser.add_argument('--max_len_generate', default=350, help='max length of outputs')
